6305f90ea00664701233b843ef24b30b5666dc68
gseonglee <jpl358@korea.ac.kr>
2024-11-16 14:45:35 +0900

    Updates

---
+++ _next/static/chunks/app/about/page-a92253b4a9cda1a1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301,695,18,437,220,895,304,732,931],{3521:function(){}},function(n){n.O(0,[971,472,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
--- _next/static/chunks/app/activities/page-9b7b2cee5f6e4547.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[18,301,695,404,437,220,931,304,732,895],{9242:function(n,e,u){Promise.resolve().then(u.t.bind(u,8326,23))}},function(n){n.O(0,[326,971,472,744],function(){return n(n.s=9242)}),_N_E=n.O()}]);
